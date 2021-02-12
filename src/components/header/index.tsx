9a70358c8524ceb05486a6fb419637c5eccafcb1
Hamza Umar Farooq Ghani <h.u.f.ghani@gmail.com>
2021-02-12 23:00:13 +0000

    add navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,57 @@
-import { Box, Flex, Heading, Link, Spacer } from '@chakra-ui/react'
+import { Box, Flex, Text, Button, Stack } from '@chakra-ui/react'
 import { Link as GatsbyLink } from 'gatsby'
 import React from 'react'
 import ThemeToggle from '../theme-toggle'
 
-interface HeaderProps {
-  siteTitle: string
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
+
+const MenuItems = (props: { [x: string]: any; children: any; isLast?: boolean; to?: string | undefined }) => {
+  const { children, isLast, to = '/', ...rest } = props
+  return (
+    <Text mb={{ base: isLast ? 0 : 8, sm: 0 }} mr={{ base: 0, sm: isLast ? 0 : 8 }} display="block" {...rest}>
+      <GatsbyLink to={to}>{children}</GatsbyLink>
+    </Text>
+  )
 }
 
-const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
-  <Box as="header" background="rebeccapurple" marginBottom="1.45rem">
-    <Box as="div" m="0 auto" maxW="960px" p="1.45rem 1.0875rem">
-      <Flex>
-        <Heading margin="0">
-          <Link as={GatsbyLink} to="/" color="white" _hover={{ textDecor: `none` }}>
-            {siteTitle}
-          </Link>
-        </Heading>
-        <Spacer />
-        <Box as="div" position="relative">
-          <ThemeToggle />
-        </Box>
-      </Flex>
-    </Box>
-  </Box>
-)
+const Header = () => {
+  const [show, setShow] = React.useState(false)
+  const toggleMenu = () => setShow(!show)
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      mb={8}
+      p={8}
+      bg={['red', 'green', 'orange']}
+      color={['blue', 'pink', 'purple']}
+    >
+      <Flex align="center">{/* <Logo w="100px" color={['white', 'white', 'primary.500', 'primary.500']} /> */}</Flex>
+
+      <Box display={{ base: 'block', md: 'none' }} onClick={toggleMenu}>
+        {show ? <CloseIcon /> : <HamburgerIcon />}
+      </Box>
+
+      <Box display={{ base: show ? 'block' : 'none', md: 'block' }} flexBasis={{ base: '100%', md: 'auto' }}>
+        <Flex
+          align={['center', 'center', 'center', 'center']}
+          justify={['center', 'space-between', 'flex-end', 'flex-end']}
+          direction={['column', 'row', 'row', 'row']}
+          pt={[4, 4, 0, 0]}
+        >
+          <MenuItems to="/">Home</MenuItems>
+          <MenuItems to="/">Home</MenuItems>
+          <MenuItems to="/pricing" isLast>
+            Pricing
+          </MenuItems>
+        </Flex>
+      </Box>
+    </Flex>
+  )
+}
 
 export default Header

72ae229475260ed086b2d68a4fb2a0b234ad64af
Hamza Umar Farooq Ghani <h.u.f.ghani@gmail.com>
2021-02-16 08:41:44 +0000

    use react hook to add in name

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,15 @@
 import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useSEOContext } from '../../contexts/seoContext'
 
-const Footer: React.FC = () => (
-  <Box as="footer" marginTop="2rem" fontSize="l" textAlign="center">
-    <Text aria-label={`© 2017 - ${new Date().getFullYear()}, Hamza Umar Farooq Ghani`} data-testid="copyRight-year">
-      © 2017 - {new Date().getFullYear()}, Hamza Umar Farooq Ghani
-    </Text>
-    
-  </Box>
-)
-
+const Footer: React.FC = () => {
+  const data = useSEOContext()
+  return (
+    <Box as="footer" marginTop="2rem" fontSize="l" textAlign="center">
+      <Text aria-label={`© 2017 - ${new Date().getFullYear()}, ${data?.author}`} data-testid="copyRight-year">
+        © 2017 - {new Date().getFullYear()}, {data?.author}
+      </Text>
+    </Box>
+  )
+}
 export default Footer

5a27dcae7854f26dd46ce920a5a96b35616a191c
Hamza Umar Farooq Ghani <h.u.f.ghani@gmail.com>
2021-05-11 21:18:37 +0100

    update/fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Flex, Heading, Image, Spacer, Text } from '@chakra-ui/react'
 import loadable from '@loadable/component'
 import React from 'react'
+import profile from './../images/profile.jpeg'
 
 const SEO = loadable(() => import(`../components/seo`))
 
 const IndexPage: React.FC = () => (
   <>
     <SEO pageTitle="Home" />
-    <Heading>Hi people</Heading>
-    <Text fontSize="xl" my={5}>
-      Welcome to your new Gatsby site.
-    </Text>
-    <Text fontSize="xl" my={5}>
-      Now go build something great.
-    </Text>
+    <Flex direction={[`column`, `column`, `row`, `row`]} justify="center" alignItems="center" width="100%">
+      <Box width={{ sm: `100%`, md: `60%`, lg: `60%` }}>
+        <Heading as="h1" textAlign={[`center`, `center`, `left`, `left`]}>
+          Hi Peaple,
+        </Heading>
+        <Heading as="h1" textAlign={[`center`, `center`, `left`, `left`]} marginBottom="2rem">
+          I'm Hamza U. F. Ghani
+        </Heading>
+      </Box>
+      <Spacer marginBottom="xs" />
+      <Box>
+        <Image borderRadius="full" boxSize="2xs" src={profile} alt="Hamza Umar farooq Ghani" border="solid 1px black" />
+      </Box>
+    </Flex>
+    <Box>
+      <Text></Text>
+    </Box>
     <Box as="div" maxWidth="300px" marginBottom="1.45rem" />
   </>
 )

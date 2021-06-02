6f565443a9c35ca4912ab2dbe660c5403ebdaea8
Hamza Umar Farooq Ghani <h.u.f.ghani@gmail.com>
2021-06-02 21:27:11 +0100

    move timeline to css-js for darkmode

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -1,14 +1,22 @@
 import { Box, Text } from '@chakra-ui/layout'
+import { useColorMode } from '@chakra-ui/react'
 import React from 'react'
 import TimelineData from '../../../config/timeline'
 import TimelineItemInterface from '../../interface/timelineItemInterface'
 import './index.css'
 
 const Timeline: React.FC = () => {
+  const { colorMode } = useColorMode()
+  const isDark = colorMode === `light` ? `Dark` : `Light`
   return (
     <>
       {TimelineData.length > 0 && (
-        <Box className="timeline-container">
+        <Box
+          className="timeline-container"
+          _after={{
+            backgroundColor: isDark === `Light` ? `#00ffbc` : `#e17b77`,
+          }}
+        >
           {TimelineData.map((data: TimelineItemInterface, idx) => (
             <TimelineItem {...data} key={idx} />
           ))}
@@ -19,15 +27,33 @@ const Timeline: React.FC = () => {
 }
 
 const TimelineItem: React.FC<TimelineItemInterface> = ({ text, date, category }) => {
+  const { colorMode } = useColorMode()
+  const isDark = colorMode === `light` ? `Dark` : `Light`
   return (
     <Box className="timeline-item">
-      <Box className="timeline-item-content">
+      <Box
+        className="timeline-item-content"
+        boxShadow={isDark === `Light` ? `0 0 5px rgba(206,187,187,0.9)` : `0 0 5px rgba(0, 0, 0, 0.2)`}
+        backgroundColor={isDark === `Light` ? `var(--chakra-colors-gray-800)` : `#fff`}
+        _after={{
+          boxShadow: isDark === `Light` ? `1px -1px 1px rgba(206,187,187,0.9)` : `1px -1px 1px rgba(0, 0, 0, 0.2)`,
+          backgroundColor: isDark === `Light` ? `var(--chakra-colors-gray-800)` : `#fff`,
+          _odd: {
+            boxShadow: isDark === `Light` ? `-1px 1px 1px rgba(206,187,187,0.9)` : `-1px 1px 1px rgba(0, 0, 0, 0.2)`,
+          },
+        }}
+      >
         <Box as="span" className="tag" style={{ background: category.color }}>
           {category.tag}
         </Box>
         <Box as="time">{date}</Box>
         <Text>{text}</Text>
-        <Box as="span" className="circle" />
+        <Box
+          as="span"
+          className="circle"
+          backgroundColor={isDark === `Light` ? `var(--chakra-colors-gray-800)` : `#fff`}
+          border={isDark === `Light` ? `3px solid #00ffbc` : `3px solid #e17b77`}
+        />
       </Box>
     </Box>
   )

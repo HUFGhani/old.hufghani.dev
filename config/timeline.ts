1ceca5e3a9d5c0bffff9c86aca8ae53998ec0b34
Hamza Umar Farooq Ghani <h.u.f.ghani@gmail.com>
2021-06-08 22:23:41 +0100

    add some informations about me in timeline

--- config/timeline.ts
+++ config/timeline.ts
@@ -24,6 +24,78 @@ export const TimelineData = [
       color: Colour.educationColour,
     },
   },
+  {
+    text: `Started college at The Manchester Collage and did a BTEC Level 2 in IT Course`,
+    date: `September 2 2009`,
+    category: {
+      tag: Tag.EDUCATION,
+      color: Colour.educationColour,
+    },
+  },
+  {
+    text: `Completed Level 2 in IT Course`,
+    date: `June 10 2010`,
+    category: {
+      tag: Tag.EDUCATION,
+      color: Colour.educationColour,
+    },
+  },
+  {
+    text: `Stayed at The Manchester Collage and did a BTEC Level 3 in National Dip in IT (Software Development) & Resit English & Maths GCSE`,
+    date: `September 6 2010`,
+    category: {
+      tag: Tag.EDUCATION,
+      color: Colour.educationColour,
+    },
+  },
+  {
+    text: `Completed BTEC Level 3 in National Dip in IT (Software Development) Course`,
+    date: `June 10 2010`,
+    category: {
+      tag: Tag.EDUCATION,
+      color: Colour.educationColour,
+    },
+  },
+  {
+    text: `Promoted Crew trainer a McDonald's`,
+    date: `July 10 2012`,
+    category: {
+      tag: Tag.WORK,
+      color: Colour.workColour,
+    },
+  },
+  {
+    text: `Stayed at The Manchester Collage and did a Foundation degree Within Computing with Database Development Accredited by Salford University`,
+    date: `September 20 2012`,
+    category: {
+      tag: Tag.EDUCATION,
+      color: Colour.educationColour,
+    },
+  },
+  {
+    text: `Started Working at Maplin`,
+    date: `April 14 2014`,
+    category: {
+      tag: Tag.WORK,
+      color: Colour.workColour,
+    },
+  },
+  {
+    text: `Left Mcdonald's`,
+    date: `August 30 2014`,
+    category: {
+      tag: Tag.WORK,
+      color: Colour.workColour,
+    },
+  },
+  {
+    text: `Left Maplin`,
+    date: `June 30 2015`,
+    category: {
+      tag: Tag.WORK,
+      color: Colour.workColour,
+    },
+  },
 ]
 
 export default TimelineData

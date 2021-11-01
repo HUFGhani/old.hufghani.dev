70de9cd301a2682bbcb9cd0b210e98c379f0f117
Hamza Ghani <h.u.f.ghani@gmail.com>
2021-11-01 22:43:00 +0000

    feat: add two pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+import SEO from '../components/SEO'
+
+const AboutMe: React.FC = () => {
+  return (
+    <>
+      <SEO pageTitle="About Me" />
+      <Heading>This page is under construction 🚧</Heading>
+      <main></main>
+    </>
+  )
+}
+
+export default AboutMe
---
+++ src/pages/project.tsx
@@ -0,0 +1,15 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+import SEO from '../components/SEO'
+
+const Project: React.FC = () => {
+  return (
+    <>
+      <SEO pageTitle="Project" />
+      <Heading>This page is under construction 🚧</Heading>
+      <main></main>
+    </>
+  )
+}
+
+export default Project

7d753da8a56f7f9c11dbfd441e68ec346b231100
Hamza Ghani <h.u.f.ghani@gmail.com>
2022-01-14 19:57:55 +0000

    refactor: markdownParser to pass sonarcloud Quality Gate

--- src/components/MarkdownParser/index.tsx
+++ src/components/MarkdownParser/index.tsx
@@ -1,50 +1,53 @@
 import { Box, Heading, ListItem, OrderedList, Text, UnorderedList } from '@chakra-ui/react'
 import React from 'react'
 
+
+type element = boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+
 const MarkdownParser = {
   h1: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => (
     <Heading as="h1" size="2xl" pb="8" textAlign="center">
       {props.children}
     </Heading>
   ),
   h2: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => (
     <Heading as="h2" size="xl" pb="6" wordBreak="break-word" textAlign="left">
       {props.children}
     </Heading>
   ),
   h3: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => (
     <Heading as="h3" size="lg" pb="4" wordBreak="break-word" textAlign="left">
       {props.children}
     </Heading>
   ),
   h4: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => (
     <Heading as="h4" size="md" pb="2" wordBreak="break-word" textAlign="left">
       {props.children}
     </Heading>
   ),
   h5: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => (
     <Heading as="h5" size="sm" pb="1" wordBreak="break-word" textAlign="justify">
       {props.children}
     </Heading>
   ),
   h6: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => (
     <Heading as="h6" size="xs" pb="1" wordBreak="break-word" textAlign="left">
       {props.children}
     </Heading>
   ),
-  p: (props: { children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined }) => (
+  p: (props: { children: element }) => (
     <Text
       as="p"
       textAlign="justify"
@@ -59,22 +62,22 @@ const MarkdownParser = {
     </Text>
   ),
   span: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => <Box as="span">{props.children}</Box>,
   ul: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => <UnorderedList>{props.children}</UnorderedList>,
   ol: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => <OrderedList>{props.children}</OrderedList>,
   li: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => <ListItem>{props.children}</ListItem>,
   blockquote: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => <Text as="blockquote">{props.children}</Text>,
   pre: (props: {
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined
+    children: element
   }) => <Text as="pre">{props.children}</Text>,
 }
 export default MarkdownParser
